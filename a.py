f7181f3d9296baa37be510b880be1462278f33c3
hossam <49634680+hossamalhossany@users.noreply.github.com>
2022-04-07 11:26:12 +0200

    8 ** 7-4-2022 @ 11:30 AM

--- a.py
+++ a.py
@@ -15,8 +15,30 @@ class Student:
         return cls(name, date.today().year - birth_year)
 
 
-a = Student
-qq = a.init_from_brith_year('hossam', 1985)
-a.describe(qq)
+class Pizza:
+    def __init__(self, ing):
+        self.ing = ing
+
+    def __str__(self):
+        return f'pizza contain  is{self.ing} '
+
+    @classmethod
+    def veg(cls):
+        return cls(['a', 'b', 'c'])
+
+    @classmethod
+    def bbb(cls):
+        return cls(['d', 'f'])
+
+
+p1 = Pizza(['y', 'ooo'])
+
+p2 = Pizza.veg()
+
+p3 = Pizza.bbb()
+
+print(p1)
+print(p2)
+print(p3)
 
 
