54d8b49c8a9bd0dc090b32089c16390c334f9020
hossam <49634680+hossamalhossany@users.noreply.github.com>
2022-04-07 11:00:01 +0200

    7 ** 7-4-2022 @ 11:00 AM

--- a.py
+++ a.py
@@ -1,34 +1,22 @@
-class Student:
-    no_of_student = 0  # class attribute
+from datetime import date
 
-    def __init__(self, name, age, courses):
+
+class Student:
+
+    def __init__(self, name, age):
         self.__name = name  # public  attribute
-        self.age = age  # private attribute
-        self.courses = courses
-        Student.no_of_student += 1  # here we use class attribute, so we must use class name first
+        self.__age = age  # private attribute
 
     def describe(self):
-        print(f'my name is {self.name} and my age ==>  {self.age}')
+        print(f'my name is {self.__name} and my age ==>  {self.__age}')
 
-    def is_old(self, num):
-        if self.age <= num:
-            print(f'this student {self.name} has age ==> {self.age} is not old than {num}')
-        else:
-            print(f'this student {self.name} has age ==> {self.age} is  old of {num}')
-
-    def get_name(self):
-        return print(self.__name)
-
-    def set_name(self, new_name):
-        self.__name = new_name
+    @classmethod
+    def init_from_brith_year(cls, name, birth_year):
+        return cls(name, date.today().year - birth_year)
 
 
-a = Student('hossam', 25, 'html')
-b = Student('waleed', 30, 'css')
-c = Student('ahmed', 35, 'java')
+a = Student
+qq = a.init_from_brith_year('hossam', 1985)
+a.describe(qq)
 
-# b.describe()
-# c.is_old(10)
 
-a.set_name('mohamed')
-a.get_name()
