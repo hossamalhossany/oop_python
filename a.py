1f95b28b3ba37aca0cf416481ed8e88630fa1500
hossam <49634680+hossamalhossany@users.noreply.github.com>
2022-04-07 09:13:00 +0200

    3 ** 7-4-2022 @ 9:12 AM

--- a.py
+++ a.py
@@ -1,1 +1,16 @@
-print('hi python ya hossam')
+class Student:
+    no_of_student = 0  # class attribute
+
+    def __init__(self, name, age, courses):
+        self.name = name
+        self.age = age
+        self.courses = courses
+        Student.no_of_student += 1
+
+
+a = Student('hossam', 25, 'html')
+b = Student('waleed', 30, 'css')
+
+
+
+print(Student.no_of_student)
