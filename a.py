59599944965cdc33ebec3c366f51d54c7dfd501f
hossam <49634680+hossamalhossany@users.noreply.github.com>
2022-04-07 10:16:21 +0200

    4 ** 7-4-2022 @ 10:12 AM

--- a.py
+++ a.py
@@ -2,15 +2,33 @@ class Student:
     no_of_student = 0  # class attribute
 
     def __init__(self, name, age, courses):
-        self.name = name
+        self.name = name  # instance attribute
         self.age = age
         self.courses = courses
-        Student.no_of_student += 1
+        Student.no_of_student += 1  # here we use class attribute, so we must use class name first
+
+    def describe(self):
+        print(f'my name is {self.name} and my age ==>  {self.age}')
+
+    def is_old(self, num):
+        if  self.age <= num:
+            print(f'this student {self.name} has age ==> {self.age} is not old than {num}')
+        else:
+            print(f'this student {self.name} has age ==> {self.age} is  old of {num}')
+
+    def get_name(self):
+        return print(self.name)
+
+    def set_name(self, name):
+        self.name = name
 
 
 a = Student('hossam', 25, 'html')
 b = Student('waleed', 30, 'css')
+c = Student('ahmed', 35, 'java')
 
+# b.describe()
+# c.is_old(10)
 
-
-print(Student.no_of_student)
+a.set_name('ali')
+a.get_name()
